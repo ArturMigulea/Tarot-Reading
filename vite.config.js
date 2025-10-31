6325e5cf6b8f732c233a82c2822538c38d5c02ee
ArturMigulea <artur.migulea@gmail.com>
2025-10-31 15:19:07 -0400

    Creating Website on Git Pages

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// OLD Vite Config
+// https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react'; // Or other framework plugin
+
+export default defineConfig({
+  base: '/Tarot-Reading.github.io/', // Replace with your actual repository name
+  plugins: [react()], // Or other framework plugins
+});
