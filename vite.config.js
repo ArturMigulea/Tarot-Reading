acb88339f15cd4117abd4e09de7e09a37282c783
ArturMigulea <artur.migulea@gmail.com>
2025-10-31 15:48:17 -0400

    Following tutorial

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// OLD Vite Config
-// https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
 export default defineConfig({
-  base: '/Tarot-Reading.github.io/', // must match your repo name exactly
   plugins: [react()],
+  base: '/Tarot-Reading',
 })
