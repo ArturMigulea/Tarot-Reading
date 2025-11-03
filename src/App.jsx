f3ce3561d21630a41229810ff043c285accebb3d
ArturMigulea <114543708+ArturMigulea@users.noreply.github.com>
2025-11-03 11:12:21 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+    <div>
       <h1>TAROT READERS</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
@@ -28,7 +20,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </div>
   )
 }
 
