4c14d557fb6d2b91c92b792f54f3b1fecd108cb1
ArturMigulea <114543708+ArturMigulea@users.noreply.github.com>
2025-11-03 12:19:17 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,22 @@ import {
   Routes, Route
 } from 'react-router-dom'
 
+// Importing the different screens
 import Title from './Screens/Title'
-import TypeSelect from './Screens/TypeSelect'
-import FateReading from './Screens/FateReading'
-import LazySusan from './Screens/LazySusan'
+// import TypeSelect from './Screens/TypeSelect'
+// import FateReading from './Screens/FateReading'
+// import LazySusan from './Screens/LazySusan'
 
 function App() {
   return (
+    // Screen Router for app navigation. Sets root URL as Github project name "Tarot-Reading"
     <Router basename="/Tarot-Reading">
+      {/* The different Screens */}
       <Routes>
         <Route path="/" element={<Title />} />
-        <Route path="/Screens/TypeSelect" element={<TypeSelect />} />
+        {/* <Route path="/Screens/TypeSelect" element={<TypeSelect />} />
         <Route path="/Screens/FateReading" element={<FateReading />} />
-        <Route path="/Screens/LazySusan" element={<LazySusan />} />
+        <Route path="/Screens/LazySusan" element={<LazySusan />} /> */}
       </Routes>
     </Router>
   )
