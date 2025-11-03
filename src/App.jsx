a5bdca28c8490d53c69bb9c71170cd795c93e459
ArturMigulea <114543708+ArturMigulea@users.noreply.github.com>
2025-11-03 11:41:16 -0500

    Implimenting Nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,29 @@
-import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
+import './styles.css'
+import {
+  BrowserRouter as Router,
+  Routes, Route, Link
+} from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const Home = () => <div><h2>Home Page</h2></div>
+  // const Notes = () => <div><h2>Notes Page</h2></div>
+  // const Users = () => <div><h2>Users Page</h2></div>
 
   return (
     <>
-      {/* <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div> */}
-      <h1>TAROT READERS</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code>Blah Blah Blah
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Blah Blah Blah
-      </p>
+      <Router>
+        <div>
+          <Link style={padding} to="/">home</Link>
+          <Link style={padding} to="/notes">notes</Link>
+          <Link style={padding} to="/users">users</Link>
+        </div>
+
+        <Routes>
+          <Route path="/notes" element={<Notes />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </>
   )
 }
