9678bf52b3157ab7ec9e05b005b332bd8a41d51f
ArturMigulea <artur.migulea@gmail.com>
2025-11-13 03:45:21 -0500

    title screen placement

--- src/Screens/Title.jsx
+++ src/Screens/Title.jsx
@@ -7,7 +7,7 @@ function Title() {
 
     return (
         <div className='ScreenContainer gypsy-bg-img bg-img'>
-            <div style={{ marginTop: "20%", display: "flex", justifyContent: "center", flexDirection: "column"}}>
+            <div style={{ marginTop: "20%", display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column"}}>
                 <h1>Ready to read your fate?</h1>
 
                 <MultiUseButton
