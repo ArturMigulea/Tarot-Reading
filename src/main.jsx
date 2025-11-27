e4c494a93c12d0fa0df3794d6df654b84b64c528
ArturMigulea <114543708+ArturMigulea@users.noreply.github.com>
2025-11-27 09:10:53 -0500

    copywrite group contributions

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,17 @@
+/*
+* ---------------------------------------------------------
+*  Project Contributors
+*  © 2025 Lilianne Boucher-Pfliger, Artur Migulea, Maxwell Innes
+* ---------------------------------------------------------
+*/
+
+console.log("Project Contributors:");
+console.log("• Lilianne Boucher-Pfliger");
+console.log("• Artur Migulea");
+console.log("• Maxwell Innes");
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
